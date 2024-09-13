bb9640e5cda2c7c28e95225de1902b24e58c5273
nadees71? <you@example.com>
2024-09-13 12:44:59 +0530

    message

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install',event => {
+    event.waitUntil(
+        caches.open('v1').then(cache => {
+            return cache.addAll([
+                '/Primecare/', // Update this path
+                '/Primecare/index.html', // Update this path
+                '/Primecare/styles/Stylehos1.css', // Update this path
+                '/Primecare/scripts/script.js', // Update this path
+                '/Primecare/favicon/favicon-32x32.png', // Update this path
+                '/Primecare/favicon/favicon-16x16.png', // Update this path
+
+            ])
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+
+        })
+    );
+});
